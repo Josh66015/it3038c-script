72981a451e65161ee4f50bdcafae72543604b81a
Josh66015 <77802491+Josh66015@users.noreply.github.com>
2022-09-25 11:39:05 -0400

    lab3

---
+++ powershell/lab3.ps1
@@ -0,0 +1,36 @@
+Function getIP{
+    (get-netipaddress).ipv4address | Select-String "192*"
+}
+
+$IP = getIP
+
+Write-Host("This machine's IP is $IP")
+
+Function TheDate{
+    (Get-Date -DisplayHint Date)
+    }
+
+$DATE = TheDate
+write-host("Todays date is $DATE")
+
+
+Function GetTheUser{
+    (Get-LocalUser -Name "Administrator")
+}
+
+$UserName = GetTheUser
+Write-Host("The User is $UserName")
+
+Function Host{
+    ([System.Net.Dns]::GetHostName())
+
+$HostName = Host
+Write-Host("Host is $HostName")
+
+Function Version{
+    ($PSVersionTable.PSVersion.ToString())
+
+$Version = Version
+Write-Host("Version of PS is $Version")
+
+Send-MailMessage -To "woodsoju@ucmail.uc.edu" -From "josh66015@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
