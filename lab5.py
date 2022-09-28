738a6bc9614209b40ee5c8dfb3156fbd35044beb
Josh66015 <77802491+Josh66015@users.noreply.github.com>
2022-09-28 09:00:40 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,12 +1,12 @@
 import datetime
 while True:
     try:
-        bday = input("Please enter your exact Date of Birth(eg.               March 6 2004):")
-        birthday = datetime.datetime.strptime(bday, '%B %d %Y')
+        birthdate = input("Please enter your exact Date of Birth(eg.               March 6 2004):")
+        birthday = datetime.datetime.strptime(birthdate, '%B %d %Y')
         break
     except:
         print("Please put in the Format 'Month Day Year' without any initial space")
 
-tday = datetime.datetime.today()
-timedelta = (tday - birthday).total_seconds()
+today = datetime.datetime.today()
+timedelta = (today - birthday).total_seconds()
 print("You have been alive for:",timedelta,"seconds")
