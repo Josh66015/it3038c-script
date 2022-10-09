08cce3dc55ddee4af6a6271be79ae0eaab12b6cb
Josh66015 <77802491+Josh66015@users.noreply.github.com>
2022-10-09 16:24:26 -0400

    project1.py

---
+++ projects/project1.py
@@ -0,0 +1,17 @@
+import datetime
+ 
+current_time = datetime.datetime.now()
+
+print("The current time is :")
+
+print(current_time)
+ 
+print("Year :", current_time.year)
+ 
+print("Month : ", current_time.month)
+ 
+print("Day : ", current_time.day)
+ 
+print("Hour : ", current_time.hour)
+ 
+print("Minute : ", current_time.minute)
